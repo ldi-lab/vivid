7ed0bbae000989488d42f016493335681a874cb7
d <215442894@qq.com>
2023-10-25 00:28:41 +0800

    chore

--- src/vivid/core/Object3D.cpp
+++ src/vivid/core/Object3D.cpp
@@ -18,7 +18,8 @@ void Object3D::AddChild(const std::shared_ptr<Object3D> &child) {
 
 
 void Object3D::LookAt(const Eigen::Vector3d &target, const Eigen::Vector3d &up) {
-    Eigen::Vector3d z = (transform_.Position() - target).normalized();
+    Eigen::Vector3d dir = transform_.Position() - target;
+    Eigen::Vector3d z = dir.normalized();
     Eigen::Vector3d x = up.cross(z).normalized();
     Eigen::Vector3d y = z.cross(x).normalized();
 
